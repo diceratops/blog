9987121abd12494074f79ce03c299094d61aa43b
diceratops <diceratops@sadie.(none)>
2011-11-14 20:28:23 -0500

    new blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '717b09bf963137b9227b1c0f6e922a9a971f8dc879314a142aec22e44379c2390b4bab920dd6dc52eadd2eb3ca6e98a4e8107204d65a6bc870a4f0e921499484'
