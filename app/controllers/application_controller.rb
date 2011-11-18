dfe6cc7bd0b02c3d09bb70bedff16efd9038761d
diceratops <diceratops@sadie.(none)>
2011-11-17 19:13:49 -0500

    updates to use redcloth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+	before_filter :find_recent_entries
+	protect_from_forgery
+	
+	def find_recent_entries
+		@recent_posts = Post.limit(3).order("created_at DESC")
+	end
+	
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.limit(5).order("created_at DESC")
 
     respond_to do |format|
       format.html # index.html.erb
