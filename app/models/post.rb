c80348359b8bf841ef93daaa4335f4b24f0bc5f9
diceratops <diceratops@sadie.(none)>
2011-12-13 13:04:16 -0500

    tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,17 +2,19 @@ class Post < ActiveRecord::Base
 	has_many :comments
 	has_many :references
 	has_many :tags, :through => :references
-	attr_accessor :tag_names	
-	after_save :assign_tags
-	def tag_names
-		@tag_names || tags.map(&:name).join(' ')
-	end	
-	private
-	def assign_tags
-		if @tag_names
-			self.tags = @tag_names.split(/\s+/).map do |name|
-			 Tag.find_or_create_by_name(name)
-			end		
-		end
-	end
+  attr_writer :tag_names
+  
+  def tag_names
+    @tag_names || tags.map(&:name).join(' ')
+  end
+  
+  private
+  
+  def assign_tags
+    if @tag_names
+      self.tags = @tag_names.split(/\s+/).map do |name|
+        Tag.find_or_create_by_name(name)
+      end
+    end
+  end
 end
